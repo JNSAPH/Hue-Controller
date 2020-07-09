1c890f31c08e3cd25b430244fe6f53bd62e5a700
JNSAPH <JNSAPH@icloud.com>
2020-07-09 11:29:24 +0200

    Temporary new features added
    - Added a way to Controll Color hue and Brightness of specific Lights
    - Removed unnecessary Code
    - Added Comments to make Code more readable
    - Slayed the kraken
    - Switched from Bootstrap CDN to Including a Version

--- src/controller.js
+++ src/controller.js
@@ -1,4 +1,4 @@
-//Import Modules and Stuff
+//Import Modules and create Variables
 const request = require('request');
 const settings = require('../settings.json')
 var IP;
@@ -9,24 +9,30 @@ request('https://discovery.meethue.com/', function (error, response, body) {
     IP = JSON.parse(body)[0].internalipaddress
     document.getElementById('uiIP').innerHTML = IP
 
-    // List Lights
+    // Get all Lamps connected to the Bridge
     request(`http://${IP}/api/${settings.username}/lights`, function (error, response, body) {
         LightList = JSON.parse(body)
 
-        // Get Number of Lights and make it usefull
-        var NumberOfLights = Object.keys(LightList)
-
-        NumberOfLights.forEach(element => {
-            // Create a Card for each Light
+        // Create a Card for every Lamp inside the System
+        /*
+            - Research if there is a better way to do this
+            - Gray-out Cards if Lamp is Offline, Unreachable or Unsupported
+        */
+        Object.keys(LightList).forEach(element => {
             document.getElementById('LightList').innerHTML +=
-                `
+            `
             <div class="card">
-                <div class="card-body">
+                <div class="card-body" data-toggle="modal" data-target="#${element}Modal">
                     <p style="font-size: 20px; font-weight: bold; text-align: center" class="md-0">${LightList[element].name}</p>
                     <p style="text-align: center" clasS="md-0">Lamp ID: ${element} | ${LightList[element].state.reachable == true ? "Online" : "Offline"}</p>
                     <div class="btn-group">
                         <button type="button" class="btn btn-success" onclick="LightSwitch(${element}, true)">On</button>
                         <button type="button" class="btn btn-danger" onclick="LightSwitch(${element}, false)">Off</button>
+                    </div>
+
+                    <div class="input-group">
+                        <input type="text" placeholder="Color (0-65535)" class="form-control" onchange="LightStateHue(${element}, this.value)">
+                        <input type="text" placeholder="Brightness (0-254)" class="form-control" onchange="LightStateBri(${element}, this.value)">
                     </div>
                 </div>
             </div>
@@ -35,16 +41,29 @@ request('https://discovery.meethue.com/', function (error, response, body) {
     });
 });
 
-function LightSwitch(lampid, on) {
+// Lamp switches
+function LightSwitch(lampid, state) {
     request.put({
         url: `http://${IP}/api/${settings.username}/lights/${lampid}/state`,
-        body: `{"on":${on}}`
+        body: `{"on":${state}}`
     })
+    console.log(`Switching Lamp ${lampid} ${on}`)
 }
 
-function LightState(lampid, bri, hue, sat) {
+function LightStateHue(lampid, hue) {
     request.put({
         url: `http://${IP}/api/${settings.username}/lights/${lampid}/state`,
-        body: `{"on":true, "sat": ${sat}, "bri": ${bri}, "hue": ${hue}}`
+        body: `{"hue": ${hue}}`
     })
+
+    console.log(`Changed Lamp ${lampid} to ${hue} Hue`)
+}
+
+function LightStateBri(lampid, bri) {
+    request.put({
+        url: `http://${IP}/api/${settings.username}/lights/${lampid}/state`,
+        body: `{"bri": ${bri}}`
+    })
+
+    console.log(`Changed Lamp ${lampid} to ${hue} Brightness`)
 }
