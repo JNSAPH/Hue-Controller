373f62bd3603ce2c49948cabc2682a314fab19cd
JNSAPH <JNSAPH@icloud.com>
2020-07-11 11:44:40 +0200

    Added Error Message
    An Error will now Occur when Hue-Controller can't find an Username in Settings.json

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const { app, BrowserWindow } = require('electron')
+const { dialog } = require('electron')
+const settings = require('./settings.json')
 
 function createWindow () {
   // Create the browser window.
@@ -13,6 +15,21 @@ function createWindow () {
 
   // Open Dev Tools
   //win.webContents.openDevTools()
+  if (settings.username == "") {
+    const options = {
+      type: 'error',
+      buttons: ['Ok'],
+      defaultId: 2,
+      title: 'An Error Occured!',
+      message: 'You dont seem to have a Username defined in settings.json',
+      detail: 'Please follow the Steps in README.md under Installation to fix this Problem.',
+    };
+  
+    dialog.showMessageBox(null, options, (response, checkboxChecked) => {
+      console.log(response);
+      console.log(checkboxChecked);
+    });
+  }
 
   // Load HTML File
   win.loadFile('views/index.html')
