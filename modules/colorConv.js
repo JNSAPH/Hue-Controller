1a5353c0d0df3588bad2a160a4255848a4ed2afe
JNSAPH <JNSAPH@icloud.com>
2020-07-15 21:53:27 +0200

    Fixed some Code Smells v3
    This is the final one i swear.

--- modules/colorConv.js
+++ modules/colorConv.js
@@ -11,24 +11,25 @@ exports.hexToRgb = (hex) => {
 }
 
 exports.xyBriToRgb = (x, y, bri) => {
-    z = 1.0 - x - y;
+    let z = 1.0 - x - y;
 
     Y = bri / 255.0; // Brightness of lamp
-    X = (Y / y) * x;
-    Z = (Y / y) * z;
-    r = X * 1.612 - Y * 0.203 - Z * 0.302;
-    g = -X * 0.509 + Y * 1.412 + Z * 0.066;
-    b = X * 0.026 - Y * 0.072 + Z * 0.962;
+    let X = (Y / y) * x;
+    let Z = (Y / y) * z;
+    let r = X * 1.612 - Y * 0.203 - Z * 0.302;
+    let g = -X * 0.509 + Y * 1.412 + Z * 0.066;
+    let b = X * 0.026 - Y * 0.072 + Z * 0.962;
     r = r <= 0.0031308 ? 12.92 * r : (1.0 + 0.055) * Math.pow(r, (1.0 / 2.4)) - 0.055;
     g = g <= 0.0031308 ? 12.92 * g : (1.0 + 0.055) * Math.pow(g, (1.0 / 2.4)) - 0.055;
     b = b <= 0.0031308 ? 12.92 * b : (1.0 + 0.055) * Math.pow(b, (1.0 / 2.4)) - 0.055;
-    maxValue = Math.max(r, g, b);
+    
+    let maxValue = Math.max(r, g, b);
     r /= maxValue;
     g /= maxValue;
     b /= maxValue;
-    r = r * 255; if (r < 0) { r = 255 };
-    g = g * 255; if (g < 0) { g = 255 };
-    b = b * 255; if (b < 0) { b = 255 };
+    r = r * 255; if (r < 0) { r = 255 }
+    g = g * 255; if (g < 0) { g = 255 }
+    b = b * 255; if (b < 0) { b = 255 }
 
     r = Math.round(r).toString(16);
     g = Math.round(g).toString(16);
@@ -40,9 +41,8 @@ exports.xyBriToRgb = (x, y, bri) => {
         g = "0" + g;
     if (b.length < 2)
         b = "0" + r;
-    rgb = "#" + r + g + b;
 
-    return rgb;
+    return "#" + r + g + b;
 }
 
 function rgbToXY(r, g, b) {
